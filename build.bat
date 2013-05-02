453ccc4c8fb402896374bc658e08b3c463d668d5
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2013-05-02 16:59:17 +0100

    Build script now creates nuget package. Switch to msbuild for build script.

--- build.bat
+++ build.bat
@@ -1,1 +1,12 @@
-@lib\Phantom\phantom.exe %*
+@echo off
+if "%1"=="PublishPackages" goto publish
+
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe build.proj %*
+goto end
+
+:publish
+powershell.exe -noprofile buildscripts\publish-nuget-packages.ps1
+
+goto end
+
+:end
